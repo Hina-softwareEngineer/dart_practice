8d206256c1ab4782de1c2159b825c632207eb0f4
HINA KHADIM <hinakhadim2002@gmail.com>
2021-04-21 18:33:21 +0500

    func in darts complete

--- functions.dart
+++ functions.dart
@@ -1,9 +1,28 @@
-import 'dart:ffi';
-
 void main() {
   helloWorld() {
     return 'hello wordld';
   }
 
   print(helloWorld());
+  // print(newfunction());
+  // print(helloDynamic());
+  // print(abc());
+  print(hello(3,4,5));
+}
+
+// String newfunction() {
+//   return 'Hina Khadim';
+// }
+
+// helloDynamic() {
+//   return {'name': "hina"};
+// }
+
+// dynamic abc() {
+//   return 4;
+// }
+
+dynamic hello(var sub1, var sub2, var sub3) {
+  int sum = sub1 + sub2 + sub3;
+  return sum;
 }
